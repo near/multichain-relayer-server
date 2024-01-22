3f9427cc8e3474b9a838a59a1a6fc085d85e8184
anthony-near <anthony@near.org>
2024-01-22 16:21:33 -0600

    tracing performance and logging

--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,34 @@ use alloy_json_rpc::{RpcClient, Request, Response};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::net::SocketAddr;
+use tower_http::trace::TraceLayer;
+use tracing::{debug, error, info, instrument, warn};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_flame::FlameLayer;
 
 // TODO refactor along with 3030 Port into config.toml and load from there
 // Constants
 const BSC_RPC_URL: &str = "https://data-seed-prebsc-1-s1.binance.org:8545";
+const FLAMETRACE_PERFORMANCE: &bool = &true;
 
 
-#[derive(Deserialize)]
+#[derive(Clone, Debug, Deserialize)]
 struct TransactionRequest {
     raw_transactions: [String; 2],
 }
 
-#[derive(Serialize)]
+#[derive(Clone, Debug, Serialize)]
 struct TransactionResponse {
     status: String,
     transaction_hash: Option<String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Clone, Debug, Deserialize)]
 struct BalanceRequest {
     account_id: String,
 }
 
-#[derive(Serialize)]
+#[derive(Clone, Debug, Serialize)]
 struct BalanceResponse {
     balance: String,
 }
@@ -40,9 +45,22 @@ struct BalanceResponse {
 
 #[tokio::main]
 async fn main() {
+    // initialize tracing (aka logging)
+    if *FLAMETRACE_PERFORMANCE {
+        setup_global_subscriber();
+        info!("default tracing setup with flametrace performance ENABLED");
+    } else {
+        tracing_subscriber::registry()
+            .with(tracing_subscriber::fmt::layer())
+            .init();
+        info!("default tracing setup with flametrace performance DISABLED");
+    }
+
     let app = Router::new()
         .route("/send_funding_and_user_signed_txns", post(handle_send_funding_and_user_signed_txns))
-        .route("/get_balance_for_account", post(handle_get_balance_for_account));
+        .route("/get_balance_for_account", post(handle_get_balance_for_account))
+        // See https://docs.rs/tower-http/0.1.1/tower_http/trace/index.html for more details.
+        .layer(TraceLayer::new_for_http());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
     println!("Listening on {}", addr);
@@ -53,7 +71,20 @@ async fn main() {
 }
 
 
+fn setup_global_subscriber() -> impl Drop {
+    let fmt_layer = tracing_subscriber::fmt::Layer::default();
 
+    let (flame_layer, _guard) = FlameLayer::with_file("./tracing.folded").unwrap();
+
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(flame_layer)
+        .init();
+    _guard
+}
+
+
+#[instrument]
 async fn handle_send_funding_and_user_signed_txns(Json(payload): Json<TransactionRequest>) -> Result<Json<TransactionResponse>, impl IntoResponse> {
     let mut client = RpcClient::new(BSC_RPC_URL.to_string());
 
@@ -73,6 +104,7 @@ async fn handle_send_funding_and_user_signed_txns(Json(payload): Json<Transactio
     }))
 }
 
+#[instrument]
 async fn handle_get_balance_for_account(Json(payload): Json<BalanceRequest>) -> Result<Json<BalanceResponse>, impl IntoResponse> {
     let mut client = RpcClient::new(BSC_RPC_URL.to_string());
 
